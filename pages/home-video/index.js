62e7b30cd5fa9688ca4ce23b799aa63e556113fb
fatJun <1870292016@qq.com>
2022-04-19 17:01:00 +0800

    创建视频详情页

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -8,14 +8,30 @@ Page({
     topMVs: [],
     hasMore: true
   },
+  //获取数据
   getTopMVData: async function (limit = 0 , reload = false) {
     if( reload ){
       this.setData({ topMVs:[], hasMore: true })
     }
+    //判断是否还有数据，没有数据则不请求
     if( !this.data.hasMore ) return
+    wx.showNavigationBarLoading()
     const res = await getMV(limit)
     console.log(res);
+    //设置数据
     this.setData({ topMVs: this.data.topMVs.concat(res.data), hasMore: res.hasMore })
+    wx.hideNavigationBarLoading()
+    //由于我们开启了loading动画，所以我们加载完成后动画也还是会在的，手动触发一下向上滑这个动作，关闭他
+    wx.stopPullDownRefresh()
+  },
+  //处理页面跳转
+  handleVideoItemClick(event){
+    console.log(event);
+    const { id } = event.currentTarget.dataset.item
+    console.log(id);
+    wx.navigateTo({
+      url: `../video-detail/index?id=${id}`,
+    })
   },
 
   /**
