b2c2c5bff0e8040fdb07e0225aa62227e87a5296
fatJun <1870292016@qq.com>
2022-04-20 17:55:38 +0800

    视频播放页请求功能、视频播放页相关视频组件

--- pages/video-detail/index.js
+++ pages/video-detail/index.js
@@ -1,66 +1,36 @@
 // pages/video-detail/index.js
+import { getMVDetail, getMVUrl, getRelatedVideo } from '../../services/api_mv'
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		mvDetails: {},
+		mvUrl: {},
+		relatedVideo: []
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-		console.log(options);
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
+		const { id } = options
+		// console.log(id);
+		this.getPageData(id)
 
 	},
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
+	//获取页面数据
+	getPageData: async function (id) {
+		const results = await Promise.allSettled([getMVDetail(id), getMVUrl(id), getRelatedVideo(id)])
+		//对应data的数据字段，promise接收后根据情况和index来修改对应的数据
+		const fieldsName = ['mvDetails', 'mvUrl', 'relatedVideo']
+		results.forEach((result, index) => {
+			console.log(result);
+			if (result.status === 'fulfilled') {
+				this.setData({ [fieldsName[index]]: result.value.data })
+			}
+		})
 	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
 })
--- services/api_mv.js
+++ services/api_mv.js
@@ -1,7 +1,41 @@
 import mRequire from './index'
+/**
+ * 获取mv列表
+ * @param {number} offset 偏移量
+ * @param {number} limit 请求数量
+ */
 export  function getMV( offset, limit = 10 ) { 
   return mRequire.get('/top/mv',{ 
     offset, 
     limit 
   }) 
 }  
+/**
+ * 获取mv详情
+ * @param {number} mvid mv的id
+ */
+export function getMVDetail(mvid) {
+  return mRequire.get('/mv/detail',{
+    mvid
+  })
+}
+
+/**
+ * 获取mv具体播放地址
+ * @param {number} id mv的id 
+ */
+export function getMVUrl(id) {
+  return mRequire.get('/mv/url',{
+    id
+  })
+}
+
+/**
+ * 获取mv相关的视频
+ * @param {number} id 相关视频
+ */
+export function getRelatedVideo(id) {
+  return mRequire.get('/related/allvideo',{
+    id
+  })
+}
