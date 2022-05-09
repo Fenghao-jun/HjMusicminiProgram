a217ab6bdf20bba1667d37f5c7f3948f1c248adc
fatJun <1870292016@qq.com>
2022-05-09 22:39:00 +0800

    巅峰榜组件

---
+++ components/rank-song-list/index.js
@@ -0,0 +1,26 @@
+// components/rank-song-list/index.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+		rankList:{
+			type:Object,
+			value:{}
+		}
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+
+	}
+})
